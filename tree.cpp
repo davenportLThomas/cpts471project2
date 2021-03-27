9cb145c86313672c8699536178597930e98c9077
davenportLThomas <thomas.davenport@wsu.edu>
2021-03-27 15:23:19 -0700

    fixed bug that was breaking tree, refactored the code

--- tree.cpp
+++ tree.cpp
@@ -60,8 +60,52 @@ class Tree{
 		return n;
 	}
 
+	void noParentString(string forThisNode)//this acts as a general node builder as well
+	{	
+		current->children.push_back(new Node);
+		Node *temp = current;
+		current = current->children[current->children.size() - 1];
+		current->object = forThisNode;
+		current->parent = temp;
+		current->node_id = nodeCount;
+		nodeCount++;
+	}
+
+	void parentString(int n)
+	{
+		//split the string
+		cout <<"Current object: " << current->object << endl;
+		string forCurrent = current->object;
+		string forParent = sequence;
+		forCurrent.erase(0, n);
+		cout << "sequence: " << sequence;
+		cout << " current: " << forCurrent << " forParent: " << forParent <<endl;
+
+		//first child settings
+		noParentString(forCurrent);	
+
+		//second child settings
+		current = current->parent;
+		noParentString("$");
+
+		//fill parent node with new string
+		current->parent->object = forParent;
+
+	}
+
 	void createChild()
 	{
+		cout << "------------------------" << endl;
+		current = findNode(root);
+		int n = matchLength(current->object);
+		cout << "n: " << n << endl;
+		if(n == 0)
+			noParentString(sequence);
+		else if(n > 0)
+			parentString(n);
+
+		sequence.erase(0,1);
+		/*
 		cout << endl;
 		cout << "---------------------------" << endl;	
 		current = findNode(root);
@@ -81,7 +125,7 @@ class Tree{
 		cout << "forCurrent: " << forCurrent << endl;	
 		cout << "n: " << n << endl;
 		string forParent = forCurrent.substr(0, n);
-		//forCurrent.erase(0, n);
+		forCurrent.erase(0, n);
 
 		cout << "current: " << forCurrent << " forParent: " << forParent <<endl;
 
@@ -101,7 +145,7 @@ class Tree{
 		}
 	
 
-		sequence.erase(0,1);
+		sequence.erase(0,1);*/
 	}
 
 	void buildTree()
@@ -122,18 +166,31 @@ class Tree{
 	Tree(string seq)
 	{
 		this->sequence = seq;
-		while(sequence.length() > 0)
+		while(sequence.length() > 0)//this should be zero, changing for testing
 			buildTree();
 		print(root);
 	}
 
+	void depthFirstSearch()
+	{
+
+
+
+	}
+
 	void print(Node *toPrint)
 	{
-		for(uint32_t i = 0; i < toPrint->children.size(); i++)
-			print(toPrint->children[i]);
+		if(toPrint->node_id != 0 )
+		{
+			for(int i = 0; i < toPrint->parent->node_id; i++)
+				cout << "   ";
+		}
 		if(toPrint->node_id != 0)//root will break things
 			cout << "Node: " << toPrint->node_id << " parent: " << toPrint->parent->node_id << " object: " << toPrint->object << endl;
-			
-		}
+		else
+			cout << "Node: 0" << endl;
+		for(uint32_t i = 0; i < toPrint->children.size(); i++)
+			print(toPrint->children[i]);
+	}
 	
 };
